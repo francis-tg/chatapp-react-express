bb826794e1846572917c25af83776b85d2672c54
francis-tg <franc9885@gmail.com>
2023-05-23 21:46:59 +0000

    change ip url

--- client/src/api/common.js
+++ client/src/api/common.js
@@ -1,1 +1,4 @@
-export const API_URL = process.env.NODE_ENV ==="production"?"https://africa-chat.onrender.com":`${window.location.protocol}//${window.location.hostname}:8000/api`
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://africa-chat.onrender.com/api"
+    : `${window.location.protocol}//${window.location.hostname}:8000/api`;
